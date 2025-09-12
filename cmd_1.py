b439df4fab11958fb767c51ddea078ad5539b8f0
Guzhavin Vitaliy <mkvk@mrpns.mrpns>
2025-09-12 15:18:17 +0300

    stage2

--- cmd_1.py
+++ cmd_1.py
@@ -1,24 +1,34 @@
 import os
+import sys
 
-VFS = r"C:\terminal"
+print("Имя скрипта:", sys.argv[0])
+print("Переданные аргументы:", *sys.argv[1:])
+
+VFS = (os.path.basename(sys.argv[1]) + os.path.dirname(__file__)) if len(sys.argv[1:]) != 0 \
+    else os.path.dirname(__file__) 
 
 while True:
-    cmd_input = input(VFS + "> ")
-    command = cmd_input.split()[0]
-    args = cmd_input.split()[1:]
-    
-    for i, arg in enumerate(args):
-        if arg[0] == "$":
-            args[i] = os.environ.get(arg[1:])
+    try:
+        cmd_input = input(VFS + "> ")
+        command = cmd_input.split()[0]
+        args = cmd_input.split()[1:]
+        
+        for i, arg in enumerate(args):
+            if arg[0] == "$":
+                args[i] = os.environ.get(arg[1:])
 
-    if command[0] == "$":
-            print(os.environ.get(command[1:]), sep = "\n")
-    elif command == "ls":
-        print(command, "\n", *args)
-    elif command == "cd":
-        print(command, "\n", *args)
-    elif command == "exit":
-        print("bye")
-        break
-    else: 
-        print("unknown command")
+        if command[0] == "$":
+                print(os.environ.get(command[1:]), sep = "\n", end = ': ')
+        if command == "ls":
+            print(command, "\n", *args)
+        elif command == "cd":
+            print(command, "\n", *args)
+        elif command == "exit":
+            print("bye")
+            break
+        else: 
+            print("unknown command")
+    except Exception as e:
+        print(f"error: {e}")
+
+
---
+++ terminal.sh
@@ -0,0 +1,1 @@
+python3 cmd_1.py VFS.tar
